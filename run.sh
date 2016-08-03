67767ad14626d734995d93256447193d1a62e521
zwjyyc <ssggjjdd@163.com>
2016-08-03 23:16:46 +0800

    Adaptive Learning

--- run.sh
+++ run.sh
@@ -1,1 +1,2 @@
-./Util /homebackup/yyc/Data/Chinese/FromYueZhang/words.dict /homebackup/yyc/Data/Chinese/FromYueZhang/words2chars.dict /homebackup/yyc/Data/Chinese/FromYueZhang/chars.dict
+make
+./cwe -train ../data/sample.txt -output-word vec.txt -output-char chr.txt -size 50 -window 5 -sample 1e-4 -negative 5 -hs 0 -iter 3 -ada 1 -threads 10
